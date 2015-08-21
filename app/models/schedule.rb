21ae80d1080a0eee6fcefe56f9e3639389c1c795
Travis Smith <mr_travis_smith@hotmail.com>
2015-08-21 20:16:14 +0000

    change indicator wording and downcase day before #determine_day_number

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,18 +2,18 @@ class Schedule < ActiveRecord::Base
 
   def recycling_week
     @todays_number = determine_day_number(Time.now.strftime("%A").downcase)
-    @recycle_day_number = determine_day_number(self.recycle_day)
+    @recycle_day_number = determine_day_number(self.recycle_day.downcase)
     @days_since_birth = (Date.today - self.birthdate).to_i
     
     week_indicator = (@days_since_birth / 7) + 1
     if self.recycle_birth_week == "y" && week_indicator.odd?
-      return false
+      return true
     elsif self.recycle_birth_week == "y" && week_indicator.even? 
-      return true
-    elsif self.recycle_birth_week == "n" && week_indicator.odd?
-      return true
-    elsif self.recycle_birth_week == "n" && week_indicator.even?
       return false
+    elsif self.recycle_birth_week == "n" && week_indicator.odd?
+      return false
+    elsif self.recycle_birth_week == "n" && week_indicator.even?
+      return true
     end
   end
   
@@ -36,14 +36,14 @@ class Schedule < ActiveRecord::Base
   end
 
   def indicate_recycling_week(recycling_week)
-    if recycling_week == true && (@recycle_day_number > @todays_number)
-      @indicator = "Recycling Day is this #{recycle_day.capitalize}."
-    elsif recycling_week == true && (@recycle_day_number < @todays_number)
-      @indicator = "Recycling Day was on #{recycle_day.capitalize}."
-    elsif recycling_week == true && (@recycle_day_number == @todays_number)
-      @indicator = "Recycling Day is today." 
+    if (recycling_week == true) && (@recycle_day_number > @todays_number)
+      @indicator = "This #{recycle_day.capitalize} is Recycling Day."
+    elsif (recycling_week == true) && (@recycle_day_number < @todays_number)
+      @indicator = "#{recycle_day.capitalize} was Recycling Day."
+    elsif (recycling_week == true) && (@recycle_day_number == @todays_number)
+      @indicator = "Today is Recycling Day." 
     else recycling_week == false
-      @indicator = "Recycling Day is next #{recycle_day.capitalize}."
+      @indicator = "Next #{recycle_day.capitalize} is Recycling Day."
     end
   end    
 
